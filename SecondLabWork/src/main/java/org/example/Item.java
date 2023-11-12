d293995e374916c234ce9d7737bc020da3694861
Maksymenko Dmytro <dmitriymaksimenko88@gmail.com>
2023-11-12 20:40:25 +0200

    Added Item class and JUnit dependency

---
+++ SecondLabWork/src/main/java/org/example/Item.java
@@ -0,0 +1,34 @@
+package org.example;
+
+abstract class Item {
+    private String title;
+    private static int uniqueID = 0;
+    private boolean isBorrowed = false;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public static int getUniqueID() {
+        return uniqueID;
+    }
+
+    public static void setUniqueID(int uniqueID) {
+        Item.uniqueID = uniqueID;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+    abstract void borrowItem();
+    abstract void returnItem();
+}
