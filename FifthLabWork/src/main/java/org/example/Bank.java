1074284ae324cf4273a9bff4b893066867f84337
Maksymenko Dmytro <dmitriymaksimenko88@gmail.com>
2023-12-03 21:40:37 +0200

    Added unitTests and fixed Bank.findAccount method

--- FifthLabWork/src/main/java/org/example/Bank.java
+++ FifthLabWork/src/main/java/org/example/Bank.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Optional;
 
 public class Bank {
-    private List<BankAccount> bankAccount = new ArrayList<BankAccount>();
+    private List<BankAccount> bankAccounts = new ArrayList<BankAccount>();
     private BankAccount sender;
     private BankAccount receiver;
 
     public void createAccount(String accountName, BigDecimal initialDeposit) throws NegativeAmountException{
         BankAccount bankAcc = new BankAccount(accountName,initialDeposit);
-        bankAccount.add(bankAcc);
+        bankAccounts.add(bankAcc);
         if (initialDeposit.compareTo(BigDecimal.ZERO)<0)
         {
             throw new NegativeAmountException("You can't initialize negative amount of funds");
@@ -20,9 +20,9 @@ public class Bank {
     }
 
     public BankAccount findAccount(int accountNumber) throws AccountNotFoundException {
-        for (BankAccount account : bankAccount) {
-            if (account.getAccountNumber() == accountNumber) {
-                return account;
+        for (int i = 0; i < bankAccounts.size(); i++) {
+            if (bankAccounts.get(i).getID() == accountNumber){
+                return bankAccounts.get(i);
             }
         }
         throw new AccountNotFoundException("Account not found");
@@ -33,10 +33,13 @@ public class Bank {
         receiver = findAccount(toAccountNumber);
 
         if (sender.getBalance().compareTo(receiver.getBalance())<=0){
-            throw new InsufficientFundsException();
+            throw new InsufficientFundsException("There are not enough funds in the account");
         }
         sender.withdraw(amount);
         receiver.deposit(amount);
     }
 
+    public List<BankAccount> getBankAccounts(){
+        return bankAccounts;
+    }
 }
